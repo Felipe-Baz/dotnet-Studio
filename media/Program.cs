ae8984a92d4edeff2da5a7db0ac5321367112169
Felipe Baz <fbazmitsuishi@gmail.com>
2020-07-16 15:30:01 -0300

    first code in dotnet

---
+++ media/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace media
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDeAlunos = 5;
+            int nota1 = 9;
+            int nota2 = 5;
+            int nota3 = 4;
+            int nota4 = 10;
+            int nota5 = 2;
+
+            int media = (nota1+nota2+nota3+nota4+nota5)/numDeAlunos;
+
+            Console.WriteLine($"A media da turma foi de {media}");
+        }
+    }
+}
